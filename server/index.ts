a6c03bd0003145bce5932e6cf6e8a2caee6d8300
JGenereux <jacegenereux@gmail.com>
2025-03-01 22:42:52 -0700

    Created files for routes and added them to the API

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,24 @@
 const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv')
+
 dotenv.config()
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
+
 app.use(cors())
+app.use(express.json())
+
+const inventoryRouter = require('./Routes/Inventory')
+const userRouter = require('./Routes/Users')
+app.use('/inventory', inventoryRouter)
+app.use('/users', userRouter)
+
+app.get('/', (req: any, res: any) => {
+    res.send('API is running')
+})
 
 app.listen(PORT,() => {
     console.log(`listening on port ${PORT}`)
