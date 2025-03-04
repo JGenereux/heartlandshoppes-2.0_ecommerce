99e9d234a16685d3f16c81300696f921ab7a1720
JGenereux <jacegenereux@gmail.com>
2025-03-04 00:09:29 -0700

    Wrote basic routes for Users route

--- server/src/Models/User.ts
+++ server/src/Models/User.ts
@@ -14,6 +14,6 @@ const userSchema = new Schema(
     }
 )
 
-const User = mongoose.model('Users', userSchema)
+const Users = mongoose.model('Users', userSchema)
 
-module.exports = User
+export {Users}
--- server/src/Routes/Users.ts
+++ server/src/Routes/Users.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import express from 'express';
 import { User } from '../Interfaces/userInterface';
 import { Bill, Order } from '../Interfaces/orderInterface';
+import { Users } from "../Models/User";
 
 const router = express.Router();
 
@@ -10,7 +11,17 @@ const router = express.Router();
  * @param {User} user The user's information
  * @returns {Number} The status code indicating if the response was successful or not 
  */
-router.post('/', async(req: Request,res: Response) => {
+router.post('/', async(req: Request,res: Response) : Promise<any> => {
+    const {user} = req.body
+
+    try{
+        const newUser = new Users(user)
+        await newUser.save()
+
+        res.status(200).json('User successfully added')
+    } catch(error) {
+        res.status(500).json(`Internal Server Error: ${error}`)
+    }
 })
 
 /**
@@ -19,8 +30,21 @@ router.post('/', async(req: Request,res: Response) => {
  * @param {Bill} billingInfo The user's new billingInfo
  * @returns {Number} The status code indicating if the response was successful or not
  */
-router.put('/:email/billing', async(req: Request,res: Response) => {
+router.put('/:email/billing', async(req: Request,res: Response): Promise<any> => {
+    const {email} = req.params
+    const {bill} = req.body
     
+    try{
+        const updatedBill = await Users.findOneAndUpdate({email: email}, {bill: bill}, {new: true})
+        
+        if(!updatedBill) {
+            return res.status(404).json('Error updating billing information')
+        }
+
+        res.status(200).json('Successfully updated billing information')
+    } catch(error) {
+        res.status(500).json(`Internal Server Error: ${error}`)
+    }
 })
 
 /**
@@ -28,7 +52,20 @@ router.put('/:email/billing', async(req: Request,res: Response) => {
  * @param {String} email The user's email
  * @returns {User} The user's information 
  */
-router.get('/:email', async(req: Request,res: Response) => {
+router.get('/:email', async(req: Request,res: Response): Promise<any> => {
+    const {email} = req.params
+
+    try{
+        const user = await Users.findOne({email:email})
+        
+        if(!user) {
+            return res.status(404).json('Error retrieving user from db')
+        }
+
+        return res.status(200).json(user)
+    } catch(error) {
+        res.status(500).json(`Internal Server Error: ${error}`)
+    }
 })
 
 /**
@@ -36,7 +73,41 @@ router.get('/:email', async(req: Request,res: Response) => {
  * @param {String} email The user's email
  * @returns {Number} The status code indicating success or error
  */
-router.delete('/:email', async(req: Request,res: Response) => {
+router.delete('/:email', async(req: Request,res: Response): Promise<any> => {
+    const {email} = req.params
+
+    try{
+        const deletedUser = await Users.findOneAndDelete({email:email})
+
+        if(!deletedUser) {
+            return res.status(404).json('Error deleting user from database')
+        }
+
+        return res.status(200).json('Successfully deleted user from database')
+    } catch(error) {
+        res.status(500).json(`Internal Server Error: ${error}`)
+    }
+})
+
+/**
+ * Retrieves all the orders of a given User
+ * @param {String} email The user's email
+ * @returns {Order[]} An array of orders containg the orderHistory of the user
+ */
+router.get('/:email/orders', async(req: Request, res: Response): Promise<any> => {
+    const {email} = req.params
+
+    try{
+        const user = await Users.findOne({email: email})
+        if(!user) {
+            res.status(404).json('Error retrieving users information from db')
+        }
+
+        const orders = user?.orderHistory
+        return res.status(200).json(orders)
+    } catch(error) {
+        res.status(500).json(`Internal Server Error: ${error}`)
+    }
 })
 
 /**
@@ -44,7 +115,25 @@ router.delete('/:email', async(req: Request,res: Response) => {
  * @param {String} email The user's email
  * @param {Order} order The order information 
  */
-router.put('/:email/orders', async(req: Request,res: Response) => {
+router.put('/:email/orders', async(req: Request,res: Response): Promise<any> => {
+    const {email} = req.params
+    const {order} = req.body
+
+    try{
+        const updatedOrders = await Users.findOneAndUpdate(
+            { email: email },
+            { $push: { orderHistory: order } },
+            { new: true }
+        );
+        
+        if(!updatedOrders) {
+            return res.status(404).json('Error updating order history for user')
+        }
+
+        return res.status(200).json("Successfully updated order history")
+    } catch(error) {
+        res.status(500).json(`Internal Server Error: ${error}`)
+    }
 })
 
 
