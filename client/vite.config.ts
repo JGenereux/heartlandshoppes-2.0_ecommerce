9d8b191c9f24b1de5c40334985f96a9458d663d8
JGenereux <jacegenereux@gmail.com>
2025-03-17 05:16:12 -0600

    Updated configs for deployment

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,11 +10,11 @@ export default defineConfig({
       usePolling: true,
     },
     host: '0.0.0.0',  // Listen on all network interfaces
-    port: 3000,  // Expose on port 3000
+    port: 80,  // Expose on port 3000
     hmr: {
       protocol: 'ws',  // Use WebSocket protocol for HMR
       host: 'localhost',  // Adjust WebSocket connection to point to localhost (the Docker host)
-      clientPort: 3000,  // Ensure the WebSocket client is on the correct port
+      clientPort: 80,  // Ensure the WebSocket client is on the correct port
     }
   }
 })
