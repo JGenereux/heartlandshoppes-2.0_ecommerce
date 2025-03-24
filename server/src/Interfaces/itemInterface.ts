0bc55808ccc6c6a72b3ebfccdda2fe7ebbf94dc3
JGenereux <jacegenereux@gmail.com>
2025-03-24 05:34:13 -0600

    Updated item model

--- server/src/Interfaces/itemInterface.ts
+++ server/src/Interfaces/itemInterface.ts
@@ -1,3 +1,6 @@
+interface priceOptions {
+    priceOptions: Record<string, number>
+}
 interface Review {
     fullName: string,
     stars: number,
@@ -12,7 +15,9 @@ interface Item {
     options: Record<string, string[]>,
     quantity: number,
     description: string,
+    priceOptions: Record<string, number>
     photos: string[],
+    isBundle: boolean,
     reviews: Review[]
 }
 export {Item}
--- server/src/Models/Item.ts
+++ server/src/Models/Item.ts
@@ -2,6 +2,12 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+const priceOptionsSchema = new Schema({
+    priceOptions: {
+        type: Map, of: Number, default: {}
+    }
+})
+
 const reviewSchema = new Schema({
     fullName: {type: String, required: true},
     stars: {type: Number, required: true},
@@ -13,11 +19,13 @@ const itemSchema = new Schema(
     {
         name: {type: String, required: true},
         price: {type: Number, required: true},
+        priceOptions: {type: priceOptionsSchema, required: false},
         category: {type: [String], required: true},
         options: { type: Map, of: [String], required: false },
         quantity: {type: Number, required: true},
         description: {type: String, required: true},
         photos: {type: [String], required: true},
+        isBundle: {type: Boolean, required: true},
         reviews: {type: [reviewSchema], required: true, default: []}
     },
     {
